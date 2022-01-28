8f3147b930317d16d5fb5911f8228cb011485e75
notroshni <97965346+notroshni@users.noreply.github.com>
2022-01-28 09:22:30 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,65 @@
+// DECLARE YOUR VARIABLES HERE
+
+var teamOneName = "Edison";
+var teamTwoName = "Bayside";
+
+var scoreOne = 0;
+var scoreTwo = 0;
+
+var freeThrow = 1;
+var twoPointShot = 2;
+var threePointShot = 3;
+
+updateTeams();
+
+let realTeamOneName, realTeamTwoName, realScoreOne, realScoreTwo, realThreePointShot, realTwoPointShot, realFreeThrow;
+
+try {
+    realTeamOneName = eval('teamOneName');
+    realTeamTwoName = eval('teamTwoName');
+    realScoreOne = eval('scoreOne');
+    realScoreTwo = eval('scoreTwo');
+    realThreePointShot = eval('threePointShot');
+    realTwoPointShot = eval('twoPointShot');
+    realFreeThrow = eval('freeThrow');
+} catch (_e) {}
+
+updateTeams();
+
+$("button").click(function(){
+    var team;
+    team = $(this).attr("team");
+   
+    var points;
+    points = $(this).attr("class");
+   
+    updateScore(team, points);
+});
+
+function updateScore(myTeam, myPoints){
+    if(myTeam === "one"){
+        if(myPoints === "twoPt"){
+            realScoreOne += realTwoPointShot;
+        } else if(myPoints === "threePt"){
+            realScoreOne += realThreePointShot;
+        } else if(myPoints === "freeThrow"){
+            realScoreOne += realFreeThrow;
+        }
+        $(".scoreOne").text(realScoreOne);
+    } else if (myTeam === "two"){
+        if(myPoints === "twoPt"){
+            realScoreTwo += realTwoPointShot;
+        } else if(myPoints === "threePt"){
+            realScoreTwo += realThreePointShot;
+        } else if(myPoints === "freeThrow"){
+            realScoreTwo += realFreeThrow;
+        }
+        $(".scoreTwo").text(realScoreTwo);
+    }
+}
+
+function updateTeams(){
+    $(".teamOne").text(realTeamOneName);
+    $(".teamTwo").text(realTeamTwoName);
+}
+
